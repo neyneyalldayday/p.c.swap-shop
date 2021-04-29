a37bfdaa908ee0f438d0129a15a4c32287eba8cb
neyneyalldayday <drfrankenstein123@gmail.com>
2021-04-29 10:43:03 -0500

    navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from '../src/components/Navbar'
+
+
+
+function App() {
+  return (
+    <>
+   <Navbar />
+    </>
+  )      
+   
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+    Nav,
+    NavbarContainer, 
+    NavLogo, 
+    
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements' 
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                    
+                </NavLogo>
+                <MobileIcon >
+                    mobile icon
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="/images">images</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/videos">videos</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/buy/sell">buy/sell</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="/forums">forums</NavLinks>
+                    </NavItem>
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+            
+        </>
+    )
+}
+
+export default Navbar
