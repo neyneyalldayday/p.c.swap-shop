6cfbcc0294140ea2046c6cf5ebe7d269f13a5a3f
neyneyalldayday <drfrankenstein123@gmail.com>
2021-06-01 20:01:20 -0500

    chunk

--- src/components/PhotoArchive/index.js
+++ src/components/PhotoArchive/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
+import PhotoArchiveCard from '../PhotoArchiveCard'
 import VisibilitySensor from 'react-visibility-sensor';
 
-export default function PhotoArchive({ images }) {  
+const PhotoArchive = ({ images }) => { 
+   
     const [imagesShownArray, setImagesShownArray] = useState(        
-        Array(images.length).fill(false) 
+        Array(images.length).fill(false)
         );
+        
      
     
 
@@ -38,23 +41,8 @@ export default function PhotoArchive({ images }) {
      );
 }
 
-function PhotoArchiveCard({ imageUrl, show }) {
-    return (
-        <div
-        className={`relative transition ease-in duration-300 transform ${
-            show ? "" : "translate-y-16 opacity-0"
-        }`}
-        >
-            <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
-            <div className="absolute inset-0 bg-black opacity-70"></div>
-            <div classNAme="mx-auto text-white z-10 self-center uppercase tracking-widest text-sm">
-                photos
-            </div>
-            </div>
-            <img src={imageUrl} alt=""/>
-        </div>
-    )
-}
+export default PhotoArchive;
+
 
 
 
---
+++ src/components/PhotoArchiveCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const PhotoArchiveCard = ({ imageUrl, show }) => {
+    return (
+        <div
+        className={`relative transition ease-in duration-300 transform ${
+            show ? "" : "translate-y-16 opacity-0"
+        }`}
+        >
+            <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
+            <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div classNAme="mx-auto text-white z-10 self-center uppercase tracking-widest text-sm">
+                photos
+            </div>
+            </div>
+            <img src={imageUrl} alt=""/>
+        </div>
+    )
+}
+
+export default PhotoArchiveCard;
