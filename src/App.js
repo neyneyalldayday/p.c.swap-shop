c637aae5fc2fc1ced7cab78295bff37aac167070
neyneyalldayday <drfrankenstein123@gmail.com>
2021-06-05 10:36:02 -0500

    debug

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from './Pages/index'
 import { Switch, Route } from 'react-router-dom';
 import GlobalStyle from './globalStyles';
 import Footer from "./components/Footer";
-import PhotoArchive from "./components/PhotoArchive";
 import VideoSpot from "./components/videos";
 import BuySell from "./components/BuySell";
 import Forums from "./components/Forums";
+import PhotoArchive from "./components/PhotoArchive";
+
+
 
 
 
@@ -20,7 +22,7 @@ function App() {
    <Navbar />
    <Switch>
      <Route path="/" exact component={Home}/>
-     <Route path="/archive" component={PhotoArchive} />
+     <Route path="/archive" render={(archiveData) => <PhotoArchive images={archiveData} />}   />
      <Route path="/videopage" component={VideoSpot} />
      <Route path="/buysellpage" component={BuySell} />
      <Route path="/forumpage" component={Forums} />
