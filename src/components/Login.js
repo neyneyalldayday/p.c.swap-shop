f8cc69e5c8480a1fc043ba8ef6146cf9f4326d34
neyneyalldayday <drfrankenstein123@gmail.com>
2021-06-07 15:55:54 -0500

    tweaked tailwind

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,11 +7,10 @@ export default function Login() {
 
     return (
         <div className="container mx-auto w-screen h-screen">
-            <div className="md:container md:mx-auto flex justify-center">
-                <h1>Login/signup</h1>
+            <div className="md:container md:mx-auto flex justify-center">               
             <div className="box-content h-64 w-64 p-4 border-4 border-green-100 shadow-2xl flex justify-center  m-20 md:m-32 flex-col relative space-y-6 bg-purple-50" >
-                <span className="block bg-green-400 flex-col  inset-x-0 top-0 m-5 rounded-full h-24 w-24 flex items-center justify-center ">Enter your id</span>
-                <input  className="block flex-col focus:outline-none focus:ring focus:border-green-100 relative m-1 " ref={idRef} placeholder="enter here"></input>
+                <span className="block bg-green-400 flex-col  inset-x-0 top-0 m-5 rounded-full h-24 w-24 flex items-center justify-center ">Login/Signup</span>
+                <input  className="block flex-col focus:outline-black focus:ring focus:border-green-100 relative m-1 placeholder-opacity-100 " ref={idRef} placeholder="enter username here"></input>
                 <button className="flex flex-col bg-green-400 m-1 rounded-full py-3 px-6 animate-bounce  transition duration-500 ease-in-out bg-green-400 hover:bg-purple-200 transform hover:-translate-y-1 hover:scale-110 ">click</button>
 
             </div>
